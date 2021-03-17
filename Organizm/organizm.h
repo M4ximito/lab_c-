dfa234daed1d9939de66fd71cb4f4688a875f257
M4ximito <65667213+M4ximito@users.noreply.github.com>
2021-03-17 13:24:39 +0300

    add organizm

--- Organizm/organizm.h
+++ Organizm/organizm.h
@@ -1,35 +1,45 @@
-#pragma once
 class Organizm {
+
 public:
-	const unsigned short limitPosilkow;
-	const unsigned short kosztPotomka;
+    const unsigned short limitPosilkow;
+    const unsigned short kosztPotomka;
+
 private:
-	unsigned short licznikZycia;
-	unsigned short licznikPosilkow;
+    unsigned short licznikZycia;
+    unsigned short licznikPosilkow;
+
 public:
-	Organizm(unsigned short dlugoscZycia,
-	 unsigned short limitPosilkow,
-	 unsigned short kosztPotomka);
-	
-	 bool zywy() const
-	 {return licznikZycia > 0; }
-	
-	 bool glodny() const
-	 {return zywy() && licznikPosilkow < limitPosilkow; }
-	
-	 bool paczkujacy() const
-	 {return zywy() && licznikPosilkow > kosztPotomka; }
+    Organizm(unsigned short dlugoscZycia,
+        unsigned short limitPosilkow,
+        unsigned short kosztPotomka);
 
-	 unsigned short stanLicznikaZycia() const
-	 {return licznikZycia; }
-	
-	 unsigned short stanLicznikaPosilkow() const
-	 {return licznikPosilkow; }
-	
-	 bool posilek();
-	 bool potomek();
-	
-	 void krokSymulacji();
-	
+    bool zywy() const
+    {
+        return licznikZycia > 0;
+    }
 
-	 };
+    bool glodny() const
+    {
+        return zywy() && licznikPosilkow < limitPosilkow;
+    }
+
+    bool paczkujacy() const
+    {
+        return zywy() && licznikPosilkow > kosztPotomka;
+    }
+
+    unsigned short stanLicznikaZycia() const
+    {
+        return licznikZycia;
+    }
+
+    unsigned short stanLicznikaPosilkow() const
+    {
+        return licznikPosilkow;
+    }
+
+    bool posilek();
+    bool potomek();
+
+    void krokSymulacji();
+};
