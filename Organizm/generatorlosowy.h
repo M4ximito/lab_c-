6cd11d4bff2797e9c7536c4db2c869d205bf19b4
M4ximito <65667213+M4ximito@users.noreply.github.com>
2021-03-24 15:01:13 +0300

    lab 4 v.2

---
+++ Organizm/generatorlosowy.h
@@ -0,0 +1,23 @@
+﻿#ifndef GENERATORLOSOWY_H
+#define GENERATORLOSOWY_H
+
+// Tutaj pójdzie cała zawartość pliku
+#include <random>
+
+class GeneratorLosowy
+{
+private:
+	static std::random_device device;
+	GeneratorLosowy() {}
+
+public:
+	static unsigned short losujPomiedzy
+	(unsigned short min, unsigned short max);
+
+	static long losujPomiedzy(long min, long max);
+	static int losujOdZeraDo(int max);
+};
+
+typedef GeneratorLosowy GEN; //Poza definicją klasy
+
+#endif // GENERATORLOSOWY_H
